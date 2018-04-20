f11773994f470ce2a9449673d3676c8eccf8d0e1
Ajith <ajithkgatty@gmail.com>
2018-04-20 13:20:07 +0530

    showing inner div for details and intial  bootstrap changes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   get 'auth/failure', to: redirect('/')
 
-  get ':username', to: 'repository#show', as: :repository
+  get ':username', to: 'repository#index', as: 'repository'
+
+  get 'repository/:id', to: 'repository#show', as: 'repo'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
