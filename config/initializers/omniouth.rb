117b126216a9c24554dfdb4763e1fb3abcb65e90
Ajith <ajithkgatty@gmail.com>
2018-04-24 20:37:30 +0530

    css changes and fixed broken outh issue

--- config/initializers/omniouth.rb
+++ config/initializers/omniouth.rb
@@ -1,4 +1,4 @@
-opts = { scope: "user,repo,gist" }
+opts = { scope: "user:email" }
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret, opts
